03726e65cac6c2ba5d0bafdf812519781c4afd19
Bryan M <allen.bry95@gmail.com>
2021-10-18 19:52:21 +0200

    feat: add cors middleware

--- app.js
+++ app.js
@@ -1,38 +1,39 @@
-const express = require('express');
-const rateLimit = require('express-rate-limit');
-const morgan = require('morgan');
-const helmet = require('helmet');
-const mongoSanitize = require('express-mongo-sanitize');
-const xss = require('xss-clean');
-// const hpp = require('hpp');
+const express = require("express");
+const rateLimit = require("express-rate-limit");
+const morgan = require("morgan");
+const helmet = require("helmet");
+const mongoSanitize = require("express-mongo-sanitize");
+const xss = require("xss-clean");
+const cors = require("cors");
 
-const AppError = require('./utils/AppError');
-const skillRouter = require('./routes/skillRoutes');
-const projectRouter = require('./routes/projectRoutes');
+const AppError = require("./utils/AppError");
+const skillRouter = require("./routes/skillRoutes");
+const projectRouter = require("./routes/projectRoutes");
 
 const app = express();
 
 //==== Middlewares
+app.use(cors());
 app.use(helmet()); // Set security HTTP headers
 
 // We want to call this middleware only on development environment
-if (process.env.NODE_ENV === 'development') app.use(morgan('dev')); // generate response logs
+if (process.env.NODE_ENV === "development") app.use(morgan("dev")); // generate response logs
 
 // Rate Limit: to prevent DoS and brute force attacks
 const limiter = rateLimit({
   max: 100, // max number of requests allowed
   windowMs: 60 * 60 * 1000, // allow "max" number request per IP / windowMs
-  message: 'Too many request from this IP. Please try again in a hour.',
+  message: "Too many request from this IP. Please try again in a hour.",
 });
 
-app.use('/api', limiter); // apply this limiter middleware only on our api
+app.use("/api", limiter); // apply this limiter middleware only on our api
 app.use(mongoSanitize()); // Data Sanitization against NoSQL Query Injection
 app.use(xss()); // Data Sanitization against XSS
 
-app.use('/api/v1/skills', skillRouter);
-app.use('/api/v1/projects', projectRouter);
+app.use("/api/v1/skills", skillRouter);
+app.use("/api/v1/projects", projectRouter);
 
-app.all('*', (req, res, next) => {
+app.all("*", (req, res, next) => {
   /*
    * If an argument is passed in next(),
    * Express will assume that there was an error
